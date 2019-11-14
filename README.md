8a7eb3e9f2d1b231c0deef7048532073258ab715
pscanf <paolo.scanferla@gmail.com>
2019-11-14 13:37:12 +0100

    feat: allow running locally without Contentful, improve docs

--- README.md
+++ README.md
@@ -1,12 +1,52 @@
+[![CircleCI](https://circleci.com/gh/pscanf/scapoli-vs-ammogliati.svg?style=svg)](https://circleci.com/gh/pscanf/scapoli-vs-ammogliati)
+
 # Scapoli vs Ammogliati
 
-Sito web per registrare e consultare i risultati di calcetto.
+Sito web ([sva.pscanf.com](https://sva.pscanf.com/)) per registrare e consultare
+i risultati di calcetto.
 
 Il sito è fatto seguendo l'architettura [JAM Stack](https://jamstack.org/),
 facendo uso dei seguenti strumenti:
 
 - **static site generator**:
   [React Static](https://github.com/react-static/react-static)
-- **headless CMS**: [Contentful](https://www.contentful.com/)
+- **content management system**: file markdown dentro questa repo, oppure
+  [Contentful](https://www.contentful.com/)
 - **server di continuous integration**: [Circle CI](https://circleci.com/)
 - **piattaforma di deploy**: [StaticDeploy](https://staticdeploy.io/)
+
+## Come modificare il sito
+
+Per il setup dell'ambiente di sviluppo:
+
+- assicurarsi di avere installati [nodejs](https://nodejs.org/) e
+  [yarn](https://yarnpkg.com/)
+- clonare questa repo
+- installare le dipendenze con `yarn install`
+
+Poi si può:
+
+- fare partire il server di sviluppo con `yarn dev`
+- buildare il sito con `yarn build`
+
+> **Nota:** per caricare i contenuti da Contenful, vanno impostate le seguenti
+> variabili d'ambiente:
+>
+> - `CONTENTFUL_SPACE_ID`
+> - `CONTENTFUL_ACCESS_TOKEN`
+
+## Report Lighthouse
+
+![report lighthouse](./lighthouseReport.png)
+
+## Link utili
+
+- [documentazione dell'architettura JAM Stack](https://jamstack.org/)
+- [lista di static site generators](https://www.staticgen.com/)
+- [lista di headless CMS](https://headlesscms.org/)
+- [tool per la valutazione delle performance di un sito (Lighthouse)](https://web.dev/measure/)
+- [repo di React Static](https://github.com/react-static/react-static/)
+- [home page Contentful](https://www.contentful.com/)
+- [home page CircleCI](https://circleci.com/)
+- [home page StaticDeploy](https://staticdeploy.io/)
+- [repo StaticDeploy](https://github.com/staticdeploy/staticdeploy/)
--- static.config.js
+++ static.config.js
@@ -1,29 +1,18 @@
 import { documentToHtmlString } from "@contentful/rich-text-html-renderer";
 import { createClient } from "contentful";
+import jdown from "jdown";
 import { omit } from "lodash";
+import { join } from "path";
 
 const websiteRootUrl = new URL(
     process.env.WEBSITE_ROOT_URL || "http://localhost:3000"
 );
 
-const contentful = createClient({
-    space: process.env.CONTENTFUL_SPACE_ID,
-    accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
-});
-
 export default {
     siteRoot: websiteRootUrl.origin,
     basePath: websiteRootUrl.pathname,
     getRoutes: async () => {
-        const gameEntries = await contentful.getEntries({
-            content_type: "game"
-        });
-        const games = gameEntries.items.map(item => ({
-            date: item.fields.date,
-            teams: item.fields.teams,
-            scores: item.fields.scores,
-            comment: documentToHtmlString(item.fields.comment)
-        }));
+        const games = await getGames();
         return [
             {
                 path: "/",
@@ -45,3 +34,32 @@ export default {
     },
     plugins: ["react-static-plugin-sitemap"]
 };
+
+async function getGames() {
+    if (
+        process.env.CONTENTFUL_SPACE_ID &&
+        process.env.CONTENTFUL_ACCESS_TOKEN
+    ) {
+        const contentful = createClient({
+            space: process.env.CONTENTFUL_SPACE_ID,
+            accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
+        });
+        const gameEntries = await contentful.getEntries({
+            content_type: "game"
+        });
+        return gameEntries.items.map(item => ({
+            date: item.fields.date,
+            teams: item.fields.teams,
+            scores: item.fields.scores,
+            comment: documentToHtmlString(item.fields.comment)
+        }));
+    } else {
+        const content = await jdown(join(__dirname, "/content"));
+        return content.games.map(game => ({
+            date: game.date,
+            teams: game.teams,
+            scores: game.scores,
+            comment: game.contents
+        }));
+    }
+}
